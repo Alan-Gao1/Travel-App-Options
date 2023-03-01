804c5afce9c887dec2e711f401e318fd6b37db25
Alan Gao <agao1@bayviewglen.ca>
2023-02-28 20:41:04 -0500

    let see if this works

---
+++ vue-school-travel-app/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import Nav from "./components/Nav.vue";
+export default {
+  components: { Nav },
+};
+</script>
+
+<template>
+  <Nav />
+  <div class="container">
+    <router-view v-slot="{ Component }">
+      <transition name = "fade" mode="out-in"> 
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
+  </div>
+</template>
+
+<style lang="css">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
